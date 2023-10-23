75aea2020636bb01de26c4b5f861e18858b9082d
grSashha <90776548+grSashha@users.noreply.github.com>
2023-10-23 14:50:41 +0300

    initial commit

---
+++ hangar_launch_vehicle_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HangarLaunchVehicleAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'hangar_launch_vehicle_app'
